2311d342b9eebbc5c2722fc41ac985a805c18367
Taher Shabbir <taherihh53@gmail.com>
2024-06-09 02:09:33 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+//assignment # 1
+// storing the person name in a variable me
+let personName = "Eric";
+// Printing  message to the person
+console.log(`Hello ${personName} would you like to learn some Python on today?`);
---
+++ main.ts
@@ -0,0 +1,9 @@
+//assignment # 1
+
+// storing the person name in a variable me
+
+let personName:string = "Eric"
+
+// Printing  message to the person
+
+console.log(`Hello ${personName} would you like to learn some Python on today?`)
